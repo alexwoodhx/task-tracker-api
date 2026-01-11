47c0e20e50e58736b1558514b0638ca027d4d8a7
alexwoodhx <woodsy366@gmail.com>
2026-01-11 19:39:59 +0000

    Wire in routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js"
+import projectRoutes from "./modules/projects/projects.routes.js";
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use("/auth", authRoutes);
-
+app.use("/projects", projectRoutes);
 app.get("/health", (req, res) => {
     res.json({ status: "ok" });
 })

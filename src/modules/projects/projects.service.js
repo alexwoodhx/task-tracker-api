f617d34745ace8f08ed11adb6c7b9a00fc08f39e
alexwoodhx <woodsy366@gmail.com>
2026-01-11 19:04:15 +0000

    Add project service

---
+++ src/modules/projects/projects.service.js
@@ -0,0 +1,53 @@
+import Project from "./project.model.js"
+
+export const createProject = async (data, userId) => {
+    return Project.create({
+        ...data,
+        owner: userId,
+    });
+};
+
+export const getProjectsForUser = async (user) => {
+    const query = user.role === "admin" ? {} : { owner: user._id};
+
+    return Project.find(query).sort({ createdAt: -1 });
+};
+
+export const getProjectById = async (projectId, user) => {
+    const query = 
+        user.role === "admin"
+            ? { _id: projectId }
+            : { _id: projectId, owner: user._id };
+
+    const project = await Project.findOne(query);
+    if (!project) throw new Error("Project not found");
+
+    return project;
+};
+
+export const updateProject = async (projectId, user, updates) => {
+    const query =
+      user.role === "admin"
+        ? { _id: projectId }
+        : { _id: projectId, owner: user._id };
+  
+    const project = await Project.findOneAndUpdate(
+      query,
+      updates,
+      { new: true, runValidators: true }
+    );
+  
+    if (!project) throw new Error("Project not found");
+  
+    return project;
+  };
+
+  export const deleteProject = async (projectId, user) => {
+    const query =
+      user.role === "admin"
+        ? { _id: projectId }
+        : { _id: projectId, owner: user._id };
+  
+    const project = await Project.findOneAndDelete(query);
+    if (!project) throw new Error("Project not found");
+  };
